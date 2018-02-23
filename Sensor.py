12171e6bcf1e87c99ba6838d7e7404940aa6634c
rnfbitto1982 <rnfchuckliddell@gmail.com>
2018-02-23 15:56:16 +0100

    Starter coding main

---
+++ Sensor.py
@@ -0,0 +1,20 @@
+#!/user/bin/env python
+
+class Sensor(object):
+    """Homesensor is a security system for pyhton"""
+
+    def __init__(self):
+        super(Sensor, self).__init__()
+
+    def main():
+        # crearsensores
+        # while True
+        #   leersensores()
+        #   if condiciones:
+        #       notificar()
+        #   sleep(1)
+        pass
+
+if __name__ == '__main__':
+    sensor = Sensor()
+    sensor.main()
