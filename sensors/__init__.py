84d4d6bd9f4feadd6f71d3490ff3b1fa09ba0800
franidn <fran95.idn@gmail.com>
2018-03-06 16:14:44 +0100

    x :x

--- sensors/__init__.py
+++ sensors/__init__.py
@@ -1,5 +1,7 @@
 from .main import Sensor
 from .binary import BinarySensor
+from .numeric import NumericSensor
 from .mocksensors import MockBinarySensor
+from .DHT11 import DHT11sensor
 
 __all__ = ["Sensor", "main", "binary", "mocksensors"]
